27827f80ade52888c4a94e456970b2ed53cf0c0b
ryoko-oono <67707600+ryoko-oono@users.noreply.github.com>
2021-02-04 11:56:31 -0800

    initial commit

---
+++ Script/app.R
@@ -0,0 +1,159 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+#install.packages("magrittr") # package installations are only needed the first time you use it
+#install.packages("dplyr")    # alternative installation of the %>%
+library(magrittr) # needs to be run every time you start R and want to use %>%
+library(dplyr) 
+library(rsconnect)
+#rsconnect::deployApp('path/to/your/app')
+
+# Define UI for application that draws a histogram
+#lophodermium_species<- as.data.frame(read.csv("/Users/ryokooono/Dropbox/R_codes_datasets/LophodermiumPhylogeny/Lophodermium_species.csv", 
+ #                                             check.names = F, header=TRUE, stringsAsFactors = F,
+  #                                            strip.white=TRUE, na.strings=c("NA", "na",
+   #                                                                          "nan", "inf", "", ".")))
+lophodermium_species <- read.csv(here("Data", "Lophodermium_species2.csv"), stringsAsFactors = FALSE)
+
+
+
+
+ui <- fluidPage(
+  titlePanel("Lophodermium species identification"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("Apothecia_length_input",
+                  "Apothecia length", min = 0, max = 2.5,
+                  value = c(0,2.5), post = "mm"),
+      sliderInput("Ascus_length_input",
+                  "Ascus length", min = 0, max = 250,
+                  value = c(0, 250), post = "um"),
+      sliderInput("Ascus_width_input",
+                  "Ascus width", min = 0, max = 45,
+                  value = c(0, 45), post = "um"),
+      sliderInput("Ascospore_length_input",
+                  "Ascospore length", min = 0, max = 200,
+                  value = c(0, 200), post = "um"),
+      radioButtons("ZoneInput", "Zone line",
+                   choices = c("Present", "Absent","Unknown"),
+                   selected = "Unknown"),
+      radioButtons("HostInput", "Host species",
+                   choices = c("Pinus", "Strobus", "Unknown"),
+                   selected = "Unknown"),
+      radioButtons("PathogenicInput", "Pathogenic status",
+                         choices = c("Pathogenic", "Endophytic"),
+                         selected = "Endophytic"),
+      radioButtons("LipInput", "Lips",
+                         choices = c("Absent", "Present","Unknown"),
+                         selected = "Unknown"),
+      checkboxGroupInput("PositionInput", "Position",
+                         choices = c("Subcuticular", "Partially subepidermal", "Subepidermal",
+                                     "Partially subhypodermal","Subhypodermal"),
+                         selected = c("Subcuticular", "Partially subepidermal", "Subepidermal",
+                                      "Partially subhypodermal","Subhypodermal"))
+    ),
+    mainPanel(
+      tableOutput("results"),
+      br(), br(),
+      plotOutput("coolplot")
+
+
+    )
+  )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  #output$coolplot <- renderPlot({
+   # filtered <-
+  #    lophodermium_species %>%
+  #    dplyr::filter(Pinus.Strobus == input$HostInput
+  #    )
+  #})
+  
+  Position <- function(x){
+      a <- data.frame()
+      b <- data.frame()
+      c <- data.frame()
+      d <- data.frame()
+      e <- data.frame()
+      f <- data.frame()
+      g <- data.frame()
+      h <- data.frame()
+      i <- data.frame()
+      
+      if ("Subcuticular" %in% input$PositionInput){
+        a <- x[which(x$position == "subcuticular"),]
+      } 
+      if ("Partially subepidermal" %in% input$PositionInput){
+        b <- x[which(x$position=="partially subepidermal"),]
+        c <- x[which(x$position=="partially subepidermal or partially subhypodermal"),]
+      }
+      if ("Subepidermal" %in% input$PositionInput){
+        d <- x[which(x$position=="subepidermal"),]
+        e <- x[which(x$position=="partially subhypodermal or subepidermal"),]
+      }
+      if ("Partially subhypodermal" %in% input$PositionInput){
+        f <- x[which(x$position=="partially subhypodermal"),]
+        g <- x[which(x$position=="partially subhypodermal or subepidermal"),]
+        e <- x[which(x$position=="partially subepidermal or partially subhypodermal"),]
+      } 
+      if ("Subhypodermal" %in% input$PositionInput) {
+      h <- x[which(x$position=="subhypodermal"),]
+      i <- x[which(x$position=="partially subhypodermal or subhypodermal"),]
+      } 
+      if ("Unknown" %in% input$PositionInput){
+        x
+      }
+      y <-rbind(a,b,c,d,e,f,g,h,i)
+      return(y)
+  }
+  
+  reactive_size <- reactive({
+      
+   lophodermium_species %>%
+                dplyr::filter(Apothecia_length_max_mm >= input$Apothecia_length_input[1]) %>%
+                dplyr::filter(Apothecia_length_min_mm <= input$Apothecia_length_input[2]) %>%
+      
+                dplyr::filter(input$Ascus_length_input[1] <= Ascus_length_max_um) %>%
+                dplyr::filter(Ascus_length_min_um <= input$Ascus_length_input[2]) %>%
+      
+                dplyr::filter(input$Ascus_width_input[1] <= Ascus_width_max_um) %>%
+                dplyr::filter(Ascus_width_min_um <= input$Ascus_width_input[2]) %>%
+      
+                dplyr::filter(input$Ascospore_length_input[1] <= Ascospore_length_max_um) %>%
+                dplyr::filter(Ascospore_length_min_um <= input$Ascospore_length_input[2]) %>%
+      
+                {if (input$HostInput=="Pinus") dplyr::filter(., Pinus.Strobus=="Pinus") else .} %>%
+                {if (input$HostInput=="Strobus") dplyr::filter(., Pinus.Strobus=="Strobus") else .} %>% 
+    
+                Position() %>% 
+      
+              #  {if (input$ZoneInput=="Present") dplyr::filter(., pathogenic=="present") else .} %>% 
+    
+                
+                {if (input$LipInput=="Absent") dplyr::filter(., Lips=="absent") else .} %>%
+                {if (input$LipInput=="Present") dplyr::filter(., Lips=="present") else .} %>%
+                
+                {if (input$PathogenicInput=="Pathogenic") dplyr::filter(., pathogenic=="pathogenic") else .} 
+    
+               
+  })
+    
+    output$results <- renderTable({
+      table<-reactive_size()
+      table[,1:10]
+    })
+    
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
---
+++ Script/test_app.R
@@ -0,0 +1,238 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(ggplot2)
+library(rgdal)
+library(leaflet)
+
+
+lambertiana ='~/Dropbox/R_codes_datasets/Pine_kml_files/lambertiana.kml'
+lambertiana.kml =readOGR(lambertiana,layer='Pinus lambertiana')
+#lambertiana.trans <- spTransform(lambertiana.kml, CRS("+proj=longlat +datum=WGS84"))
+#lambertiana.fort <- fortify(lambertiana.trans)
+
+
+monticola ='~/Dropbox/R_codes_datasets/Pine_kml_files/monticola.kml'
+monticola.kml =readOGR(monticola,layer='Pinus monticola')
+
+#monticola.trans <- spTransform(monticola.kml, CRS("+proj=longlat +datum=WGS84"))
+#monticola.fort <- fortify(monticola.trans)
+
+#plot(monticola.kml)
+
+lambertiana_points<- as.data.frame(read.csv("/Users/ryokooono/Dropbox/R_codes_datasets/LophodermiumGenome/Locations_lambertiana.csv", 
+                                            check.names = F, header=TRUE, stringsAsFactors = T,
+                                            strip.white=TRUE, na.strings=c("NA", "na",
+                                                                           "nan", "inf", "", ".")))
+monticola_points<- as.data.frame(read.csv("/Users/ryokooono/Dropbox/R_codes_datasets/LophodermiumGenome/Locations_monticola.csv", 
+                                          check.names = F, header=TRUE, stringsAsFactors = T,
+                                          strip.white=TRUE, na.strings=c("NA", "na",
+                                                                         "nan", "inf", "", ".")))
+lophodermium_species<- as.data.frame(read.csv("/Users/ryokooono/Dropbox/R_codes_datasets/LophodermiumPhylogeny/Lophodermium_species.csv", 
+                                              check.names = F, header=TRUE, stringsAsFactors = T,
+                                              strip.white=TRUE, na.strings=c("NA", "na",
+                                                                             "nan", "inf", "", ".")))
+
+species_list <- list("lambertiana.kml", "monticola.kml")
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  titlePanel(HTML("Open-access real-time phylogenetic analyses of pine-specific <em>Lophodermium</em>")),
+  h2("Introduction"),
+  "This is a working draft of a dynamic phylogeny and collection database for the pine-associated genus", 
+  em("Lophodermium."), 
+  "This cyber-platform integrates text, code, and data in the same document 
+  as a shiny web app with a webserver that is running R in the background on top of a specimen-based database 
+  to generate all formatted figures and analyses presented here. There are multiple advantages to this real-time 
+  monograph: i) it is perpetually updated with the curated database of new specimens and sequences deposited to XXX, or when there are updates to the current database (ie., specimen metadata change), R is launched on 
+  a computer server and all scripts are activated to re-generate new text, plots, and results; ii) all analyses are stream-lined
+  and reproducible for consistency and transparency of taxonomic positions and descriptions; and iii) all data are free to use, so other 
+  users can conduct new analyses or download the data for other purposes.
+  We started conceptualizing this model in 2018, outlined in an NSF EAGER proposal with Co-PIs Katja Seltmann and Michelle O'Malley, 
+  to build a comprehensive and real-time analytical platform for all the information on endophyte diversity. To begin, 
+  we focus on one fungal genus, but the goal is to develop a template for any study system. 
+  This cyberplatform is the hub that synthesizes all the information and knowledge about a focal taxonomic group and is effectively 
+  the interface to the database. Because knowledge is not static, it will keep growing and changing as we understand 
+  better the biology of these endophytes. The execution of this idea in a shiny app was further inspired by 
+  Felipe Zapata's electronic dynamic monograph of", em("Escallonia."), "Contributions, ideas, and suggestions are welcome, 
+  please send me an email ryoko.oono@lifesci.ucsb.edu",
+  
+  h2("Lophodermium"),
+  em("Lophodermium"), "Chevall. is a paraphyletic genus in Rhytismataceae (Rhytismatales, Leotiomycetes) 
+                with more than 100 species described. They are all associated with either living or dead plants 
+                as endophytes or saprophytes but can be found on a diverse range of hosts, from grasses to conifers 
+                (e.g. Lantz et al. 2011). Currently, around 38 Lophodermium species, known to be endophytic with", 
+  em("Pinus"),
+  "(pine) species, comprise a group hypothesized to be monophyletic (Ortiz-García et al. 2003). 
+                Despite the smaller number of species, the classification of pine-associated", 
+  em("Lophodermium"), "is challenging 
+                because the few, relatively simple morphological characters are difficult to interpret. Some morphological 
+                characters, such as subcuticular vs. subepidermal ascocarps (Minter 1981), have converged, leading to 
+                misidentifications and hindering the ability of mycologists to identify species without sequencing 
+                (Minter 1981, Ortiz-García et al. 2003). These species form small ascomata (< 5 mm) on plant litter that 
+                require multiple parallel cross-sectioning to reveal diagnostic morphological characters under the microscope 
+                and culture descriptions are rare. Also, there is relatively poor sampling to identify any geographical or 
+                ecological patterns that would help delineate different species, in addition to morphological characters. ",
+  br(),
+  h2("Geographic distribution"),
+  em("Lophodermium"), "is distributed worldwide but most well-studied in regions with high densities of pines (ca. 115 species). 
+                Hence, the majority of work on",em("Lophodermium"), "come from the northern hemisphere, primarily North America, China, or Europe. However, they have 
+                been found from pine plantations in the southern hemisphere, such as New Zealand. The following is a map of species ranges of pine species 
+                (if available by kml) as well as known collection sites of",
+  em("Lophodermium"),  "specimens deposited in GenBank or published in peer-reviewed journals. Colors of points match color of host species range. 
+                Shape of point corresponds to", em("Lophodermium"), "species. Click on each point for links to GenBank page and other metadata.",
+  p(), 
+  
+   # Application title
+  mainPanel( 
+    #this will create a space for us to display our map
+    leafletOutput(outputId = "mymap"), 
+    #this allows me to put the checkmarks ontop of the map to allow people to view earthquake depth or overlay a heatmap
+    absolutePanel(top = 60, left = 20, 
+                  checkboxGroupInput(inputId = "pine_species", "Species ranges to see:",
+                                     choiceNames =
+                                       list("Pinus lambertiana","Pinus monticola"),
+                                     choiceValues =
+                                       list("lambertiana.kml", "monticola.kml"),
+                                     selected = NULL
+                  ),
+                  verbatimTextOutput("checkBoxText")
+                                 
+    )
+  ),
+  p(),
+  h2("Interactive morphological guide for species identification"),
+  p("While dichotomous keys are commonly applied to narrow down species identity, not all traits are easily measured or recognizable by non-specialists.
+    Here, we provide an interactive key with visual guides that does not have to follow the traditional dichotomous hierarchy in case people want to skip
+    over some traits. "),
+  
+  fluidRow(
+    column(4,
+           selectInput("subgenus",
+                       "Host subgenus:",
+                       c("All",
+                         unique(as.character(lophodermium_species$Host_subgenus))))
+    ),
+  
+    column(4,
+           selectInput("ascocarp",
+                       "Ascocarp position:",
+                       c("All",
+                         unique(as.character(lophodermium_species$Ascocarp_position))))
+    )
+  ),
+  # Create a new row for the table.
+  DT::dataTableOutput("table"),
+  h2("Phylogenetic history"),
+  p("The internal transcribed spacer and large 16S rDNA subunit are the most commonly sequenced markers for Fungi and "),
+  em("Lophodermium."), "We provide here the majority-rule consensus Bayesian phylogeny published in Salas Lizana & Oono (2017). ",
+  # Sidebar with a slider input for number of bins 
+  h2("Microbiome of groups of pine individuals"),
+  
+  p("Analysis of microbial communities requires the interpretation of one or more high-dimensional abundance matrices and its relationship with other datasets, using a complex and emerging suite of methods from ecology, genetics, phylogenetics, multivariate statistics, visualization and testing. Filtering, custom curation and transformation of the abundance data are also required usually, but the precisely reproducible workflow from raw data to final analyses is often difficult or impossible to reproduce exactly. Ideally, published scientific analyses are completely reproducible in as easy a fashion as possible; and anything less represents an impediment to both progress and peer review - Peng, 2011, McMurdie & Holmes 2015"),
+  "Here, we take advantage of the Shiny-phyloseq app and present various analyses that can be performed interface curation of OTU tables after selecting for host samples of interest by the user."
+  
+
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+   
+     output$mymap <- renderLeaflet({
+      leaflet(lambertiana_points) %>% 
+         setView(lng = -129, lat = 45, zoom = 3)  %>% #setting the view over ~ center of North America
+         addTiles() %>% 
+         addCircles(data = lambertiana_points, lat = ~ Lat, lng = ~ Long, 
+                    weight = 1, radius = ~sqrt(Fiss_S)*25000, popup = ~as.character(Fiss_S), 
+                    label = ~as.character(paste0("Magnitude: ", sep = " ", Fiss_S)), 
+                    #color = ~pal(mag), 
+                    fillOpacity = 0.5)
+     })
+     
+   observe({
+        proxy <- leafletProxy("mymap")
+        if(is.null(input$pine_species)){
+          proxy %>%  clearShapes()
+          output$checkBoxText <- renderText({length(input$pine_species)})
+          } 
+        else{
+          for (i in 1:length(input$pine_species)){
+            if (i == 1){
+                        new_proxy <- proxy %>% 
+                          registerPlugin(kmlPlugin) %>%
+                          addPolygons(data = input$pine_species[i], # CHANGE cases by variableplot
+                                     color = "green",
+                                      dashArray = "3",
+                                      fillOpacity = 0.5) 
+               } else{
+              new_proxy <- new_proxy %>% 
+                          registerPlugin(kmlPlugin) %>%
+                          addPolygons(data = input$pine_species[i], # CHANGE cases by variableplot
+                                      color = "green",
+                                      dashArray = "3",
+                                      fillOpacity = 0.5) 
+       
+          output$checkBoxText <- renderText({length(input$pine_species)})
+          
+        }
+            new_proxy
+        }
+        }
+        
+   })
+#
+#    if (input$pine_species %in% species_list){
+#proxy
+            #      for(i in 1:length(input$pine_species)){
+   #         if (i == 1){
+  #          new_proxy <- proxy %>% 
+  #            registerPlugin(kmlPlugin) %>%
+  #            addPolygons(data = input$pine_species[i], # CHANGE cases by variableplot
+  #                        color = "green",
+  #                        dashArray = "3",
+  #                        fillOpacity = 0.5) 
+  #          } else{
+  #          new_proxy <- new_proxy %>% 
+  #            registerPlugin(kmlPlugin) %>%
+  #            addPolygons(data = input$pine_species[i], # CHANGE cases by variableplot
+  #                        color = "green",
+  #                        dashArray = "3",
+  #                        fillOpacity = 0.5) 
+  #          }
+  #      }
+  #    new_proxy
+  #      } else {
+  #        proxy %>%  clearShapes() %>% 
+  #          addCircles(data = lambertiana_points, lat = ~ Lat, lng = ~ Long, 
+  #                     weight = 1, radius = ~sqrt(Fiss_S)*25000, popup = ~as.character(Fiss_S), 
+  #                     label = ~as.character(paste0("Magnitude: ", sep = " ", Fiss_S)), 
+  #                     #color = ~pal(mag), 
+  #                     fillOpacity = 0.5)
+  #      }
+  #     })
+
+       
+     
+     output$table <- DT::renderDataTable(DT::datatable({
+       data <- lophodermium_species
+       if (input$subgenus != "All") {
+         data <- data[data$Host_subgenus == input$subgenus,]
+       }
+       if (input$ascocarp != "All") {
+         data <- data[data$Ascocarp_position == input$ascocarp,]
+       }
+       data
+     }))
+  
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
